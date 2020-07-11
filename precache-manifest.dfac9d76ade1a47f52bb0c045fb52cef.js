579a8a955514ffcf09582a28acc80b91877d7b57
foxxycat <43220582+foxxycat@users.noreply.github.com>
2020-07-11 16:27:19 +0300

    Updates

---
+++ precache-manifest.dfac9d76ade1a47f52bb0c045fb52cef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbbb0b2509a0637fdd318d31a6048af4",
+    "url": "/spicy-app/mini-app/index.html"
+  },
+  {
+    "revision": "59ce04c2998410c56646",
+    "url": "/spicy-app/mini-app/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "3b1e6ad64cca4c03a116",
+    "url": "/spicy-app/mini-app/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "59ce04c2998410c56646",
+    "url": "/spicy-app/mini-app/static/js/2.7763685a.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/spicy-app/mini-app/static/js/2.7763685a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b1e6ad64cca4c03a116",
+    "url": "/spicy-app/mini-app/static/js/main.c39767f7.chunk.js"
+  },
+  {
+    "revision": "1bc93b8b211a5d3f2102",
+    "url": "/spicy-app/mini-app/static/js/runtime-main.2e5b1c82.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/spicy-app/mini-app/static/media/persik.4e1ec840.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/spicy-app/mini-app/precache-manifest.dfac9d76ade1a47f52bb0c045fb52cef.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spicy-app/mini-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
