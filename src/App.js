58750d59752d668bec910cd8f0caeda4222dce21
foxxycat <43220582+foxxycat@users.noreply.github.com>
2020-07-12 13:04:25 +0300

    retest

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import View from '@vkontakte/vkui/dist/components/View/View';
 import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
 import '@vkontakte/vkui/dist/vkui.css';
 
+// Импорт страниц
 import Home from './panels/Home';
 import Persik from './panels/Persik';
+import Register from './panels/Register';
+
 import { Epic, Tabbar, TabbarItem, Panel, PanelHeader } from '@vkontakte/vkui';
 
 const App = () => {
-	const [activePanel, setActivePanel] = useState('Register');
+	const [activePanel, setActivePanel] = useState('Register'); // сменил useStage был Home
 	const [fetchedUser, setUser] = useState(null);
 	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
 
